1464184aa772cfade2028741efed7b2b64060e1d
Tunde Balogun <tuns4fun@gmail.com>
2026-07-10 15:50:09 +0100

    blogs and model creation

---
+++ blogs/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Category, Blogs
+
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('id', 'title', 'category', 'author', 'status', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'status')
+    list_editable = ('is_featured',)
+
+
+admin.site.register(Category)
+admin.site.register(Blogs, BlogAdmin)
+
+
+
+
+
