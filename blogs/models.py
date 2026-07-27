5dee7ffc14cc69bc15ac46d5ae18788ef4b23836
Tunde Balogun <tuns4fun@gmail.com>
2026-07-27 18:12:54 +0100

    Update blog_body field configuration

--- blogs/models.py
+++ blogs/models.py
@@ -28,7 +28,7 @@ class Blogs(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
     short_description = models.TextField(max_length=1000)
-    blog_body = models.TextField(max_length=3000)
+    blog_body = models.TextField(max_length=4000)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Draft")
     is_featured = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
